6fb02b11164aada2278cbd579519d00e383fe3f7
danielsg27 <109993994+danielsg27@users.noreply.github.com>
2022-08-01 12:49:55 -0500

    Update ROI_Binarizada.py

--- Rx_Codigo/ROI_Binarizada.py
+++ Rx_Codigo/ROI_Binarizada.py
@@ -1,6 +1,13 @@
 import cv2
 import numpy as np 
 import matplotlib.pyplot as plt
+import os
+
+main_path = '/home/danielsg27/Documentos/Caps/Pruebas_LuzAmbiente/'
+path = main_path + 'Capturas_LuzAmbiente/'
+filenames = os.listdir(path)
+header = 'cap_2022_01_01_'
+
 
 # ROI = x,y w,h
 # 200,260,700,80
@@ -11,44 +18,20 @@ y = 260
 w = 440 #440
 h = 80
 
+
 kernel = np.ones((5, 5), np.uint8)
 
-im=np.array(cv2.imread("/home/danielsg27/Documentos/Capturas/IMG_0003.jpg"))
-im2= im[y:y+h, x:x+w]
-redim = cv2.resize(im2, (700,len(im2)))
+for file in filenames:
+    im = cv2.imread(path+file)
+    im2= im[y:y+h, x:x+w]
+    redim = cv2.resize(im2, (700,len(im2)))
+    imgGray = cv2.cvtColor(redim, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5,5),cv2.BORDER_ISOLATED)
+    ret, th = cv2.threshold(imgBlur,75,255,cv2.THRESH_BINARY) #80
+    imBin=np.array(th)
+    
+    
+    cv2.imwrite(main_path+"Binarizadas_LuzAmbiente/ROI_BIN_"+file,th)
 
-"""imgGray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgGray, (5,5), 10)
-ret, th = cv2.threshold(imgBlur,100,255,cv2.THRESH_BINARY) #30-50  #obtu 200 con 75
-"""
-imgGray = cv2.cvtColor(redim, cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgGray, (5,5),cv2.BORDER_ISOLATED)
-ret, th = cv2.threshold(imgBlur,80,255,cv2.THRESH_BINARY) #30-50  #obtu 200 con 75
-
-
-
-
-
-cv2.imshow('Binarizada', th)
-
-#imgHSV = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-imgGray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-imgBin = cv2.threshold(imgGray, 127, 255, cv2.THRESH_BINARY)
-
-imgBlur = cv2.GaussianBlur(imgGray, (5,5), 1)
-imgCanny = cv2.Canny(th, 100, 320)
-imgDialation = cv2.dilate(imgCanny, kernel, iterations=1)
-imgEroded = cv2.erode(imgDialation, kernel, iterations=1)
-#im2= im[490:550, 200:900]
-#im2= im[x:x+w, y:y+h]
-#cv2.imshow('Binary',imgBin)
-#cv2.imshow('imagen2',im2)
-
-cv2.imwrite("/home/danielsg27/Documentos/Capturas/Capturas_ROI/ROI_1.jpg",im2)
-#cv2.imwrite("/media/danielsg27/0C78-D0E6/Pruebas_OCC/Capturas/Capturas_ROI/ROI_1_HSV.jpg",imgHSV)
-cv2.imwrite("/home/danielsg27/Documentos/Capturas/Capturas_ROI/ROI_1_GRAY.jpg",imgGray)
-cv2.imwrite("/home/danielsg27/Documentos/Capturas/Capturas_ROI/ROI_1_BIN.jpg",th)
-cv2.imwrite("/home/danielsg27/Documentos/Capturas/Capturas_ROI/ROI_1_Dia.jpg",imgDialation)
-cv2.imwrite("/home/danielsg27/Documentos/Capturas/Capturas_ROI/ROI_1_Canny.jpg",imgCanny)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
